a47ecef0bb82eaf8de570b862329a0758b0a294a
Haritz Puerto <haritzpuerto94@gmail.com>
2020-05-06 02:47:08 +0000

    added setup.sh
    for convenience when cloning the repository

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # Guided Regularization between Copy and Paraphrase for Unsupervised Question Generation
 
-### Download QG modules
-LM: http://143.248.48.90:8080/shared/filebrowser/?token=e55f64532f5bafb00d25ac1f922b8215229f3971
-
-UQA: http://143.248.48.90:8080/shared/filebrowser/?token=848397cc85a87771228cb2435958e32837032ace
-
-Once you download two models, move them to correct directories as follows.
-
-    mkdir BERTQG/models/lm_10k_QG
-    mkdir BERTQG/models/uqa_10k_QG
-    mv pytorch_model.bin BERTQG/models/lm_10k_QG/
-    mv pytorch_model.bin BERTQG/models/uqa_10k_QG/
+Excute ./setup.sh to download the trained models
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Downloading UQA 10K model..." 
+
+wget -O pytorch_model.bin https://s3.us-west-2.amazonaws.com/secure.notion-static.com/2ee97f79-b017-4bed-a8fa-1894073aba32/uqa_10k_QG.bin\?X-Amz-Algorithm\=AWS4-HMAC-SHA256\&X-Amz-Credential\=AKIAT73L2G45O3KS52Y5%2F20200506%2Fus-west-2%2Fs3%2Faws4_request\&X-Amz-Date\=20200506T022553Z\&X-Amz-Expires\=86400\&X-Amz-Signature\=8f795cb034a4008b43f296be365d38bb902c98d8e0155c9d8fe9ba8ab566a7a5\&X-Amz-SignedHeaders\=host\&response-content-disposition\=filename%20%3D%22uqa_10k_QG.bin%22
+
+mkdir BERTQG/models/uqa_10k_QG
+mv pytorch_model.bin BERTQG/models/uqa_10k_QG/
+
+
+echo "/n#################################"
+echo "Downloading LM 10K model..." 
+wget -O pytorch_model.bin https://s3.us-west-2.amazonaws.com/secure.notion-static.com/4c8fd5f3-0efd-41b0-ad87-d591d40fc9e0/lm_10k_QG.bin\?X-Amz-Algorithm\=AWS4-HMAC-SHA256\&X-Amz-Credential\=AKIAT73L2G45O3KS52Y5%2F20200506%2Fus-west-2%2Fs3%2Faws4_request\&X-Amz-Date\=20200506T022149Z\&X-Amz-Expires\=86400\&X-Amz-Signature\=bd26c7018321d8c1ba1829b188c38980c2cfacdfd9896edcdce78da0f43b396e\&X-Amz-SignedHeaders\=host\&response-content-disposition\=filename%20%3D%22lm_10k_QG.bin%22
+
+mkdir BERTQG/models/lm_10k_QG
+mv pytorch_model.bin BERTQG/models/lm_10k_QG/
+
+
+
